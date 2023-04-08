763a59d7518b55ffc485f84073e1e57e820378c3
vid-d <sachinviduranga@icloud.com>
2023-04-08 14:38:35 +1200

    Test

--- client.py
+++ client.py
@@ -7,14 +7,11 @@ class SecureClient:
         self.port = port
 
     def connect(self):
-        context = ssl.create_default_context()
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
         with socket.create_connection((self.host, self.port)) as sock:
-            with context.wrap_socket(sock, server_hostname=self.host) as ssock:
+            with ssl.create_default_context(ssl.Purpose.SERVER_AUTH).wrap_socket(sock, server_hostname=self.host) as ssock:
                 ssock.sendall(b"Hello, server!")
                 data = ssock.recv(1024)
                 print(f"Received from server: {data.decode()}")
 
-client = SecureClient(host="localhost", port=888)
+client = SecureClient(host="localhost", port=443)
 client.connect()
