399bdc45032603eddbecc1fb2d71b8d44a7cf7d4
vid-d <sachinviduranga@icloud.com>
2023-04-09 15:49:46 +1200

    Test

--- client.py
+++ client.py
@@ -8,12 +8,12 @@ class Client:
     def connect(self):
         with socket.create_connection((self.host, self.port)) as sock:
             print(f"Connected to {self.host}:{self.port}")
-            sock.sendall(b"Hello, server!")
+            sock.sendall(b"{workflow_id:1}")
             print("Sent message to server")
             data = sock.recv(1024)
             print(f"Received from server: {data.decode()}")
             
-client = Client(host="3.86.198.38", port=443)
+client = Client(host="3.86.198.38", port=6000)
 try:
     client.connect()
 except Exception as e:
