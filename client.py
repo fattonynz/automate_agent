45d1f48002e74013502224ecd3ec331e2ac357af
vid-d <sachinviduranga@icloud.com>
2023-04-09 19:14:01 +1200

    Test

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ class Client:
     def connect(self):
         with socket.create_connection((self.host, self.port)) as sock:
             print(f"Connected to {self.host}:{self.port}")
-            sock.sendall(b"{workflow_id:1}")
+            sock.sendall(b"{flow_id:1}")
             print("Sent message to server")
             data = sock.recv(1024)
             print(f"Received from server: {data.decode()}")
